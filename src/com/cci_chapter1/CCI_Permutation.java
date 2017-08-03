a36057c4af4dc19f72a92de86a5b91a292559a93
kshithija <kshithijakappa@gmail.com>
2017-08-02 23:53:10 -0600

    Update CCI_Permutation.java

--- src/com/cci_chapter1/CCI_Permutation.java
+++ src/com/cci_chapter1/CCI_Permutation.java
@@ -1,3 +1,22 @@
+
+/*
+Check Permutation: Given two strings, write a method to decide if one is a permutation of the
+other. 
+*/
+/*
+Like in many questions, we should confirm some details with our interviewer. We should understand if the
+permutation comparison is case sensitive. That is: is God a permutation of dog? Additionally, we should
+ask if whitespace is significant. We will assume for this problem that the comparison is case sensitive and
+whitespace is significant. So, "god " is different from "dog".
+Observe first that strings of different lengths cannot be permutations of each other. There are two easy
+ways to solve this problem, both of which use this optimization.
+Solution #1: Sort the strings.
+If two strings are permutations, then we know they have the same characters, but in different orders. Therefore,
+sorting the strings will put the characters from two permutations in the same order. We just need to
+compare the sorted versions of the strings. 
+
+*/
+
 import java.util.Scanner;
 
 public class CCI_Permutation {
